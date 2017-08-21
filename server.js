c7c68796517cc3d2f51c42059de59f9b71b325e7
gtbmed <jkbarson@gmail.com>
2017-08-20 20:47:11 -0400

    update server.js for sequelized style routing and removed orm.js file after replacing with models/burger.js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const db = require('./models');
 
 var port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Routes
+require("./routes/burer-api-routes.js")(app);
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controllers.js");
-
-app.use("/", routes);
-
-app.listen(port);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
