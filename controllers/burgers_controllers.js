a35f13e339e73cd20185a8249370e7bda3db7798
gtbmed <jkbarson@gmail.com>
2017-08-20 22:12:42 -0400

    PUT method updated to sequelized version

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,12 +1,12 @@
 // Dependencies
 const express = require('express');
-const burger = require('../models/burger.js')["Burger"]; // Route to sequelized Burger
+const db = require('../models/'); // Route to sequelized Burger
 // Create a routher using express
 var router = express.Router();
 
 // Route for getting all burger info stored in db
 router.get("/", function(req, res) {
-  Burger.findAll({}).then(function(data) {
+  db.Burger.findAll({}).then(function(data) {
     console.log(data);
     res.render("index", data);
   });
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
 
 // Route for posting new burger / Creating a new burger
 router.post("/burger/create", function(req, res) {
-  Burger.create({
+  db.Burger.create({
     burger_name: req.body.burger_name
   }).then(function(addBurger) {
     console.log(addBurger);
@@ -22,10 +22,14 @@ router.post("/burger/create", function(req, res) {
   });
 });
 
-***// Route for update a burger status to "devoured"
+// Route for update a burger status to "devoured"
 router.put('/burger/update/:id', function (req, res) {
-  var condition = "id = " + req.params.id;
-  burger.update({"devoured" : req.body.devoured}, condition, function(data) {
+  db.burger.update({
+    devoured: req.body.devoured
+  }, {
+  where: {
+    id: req.params.id
+  }).then(function() {
     res.redirect("/");
   });
 });
